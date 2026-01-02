391db564ffe019305bf7a2cdfa63caa991bbfad4
Aiteeedet <145389470+Aiteeedet@users.noreply.github.com>
2026-01-02 10:18:26 +0100

    Add main App component with FlowrateWalletAndData

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import FlowrateWalletAndData from "./components/FlowrateWalletAndData";
+
+export default function App() {
+  return (
+    <div>
+      <h1>Flowrate</h1>
+      <FlowrateWalletAndData />
+    </div>
+  );
+}
